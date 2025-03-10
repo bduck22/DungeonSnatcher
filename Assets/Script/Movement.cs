69fb0cc8dbee6c7c28a74d9d10fa93b88ff5e4fb
bduck22 <hwangwb1224@hanmail.net>
2025-03-10 14:05:56 +0900

    0310-0205

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,6 +4,7 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private int Speed;
     [SerializeField] private int JumpPower;
+    [SerializeField] private bool JumpTrigger;
     void Start()
     {
         PlayerPrefs.SetInt("JumpKey",308);
@@ -12,14 +13,36 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Speed*Time.deltaTime, 0, 0);
+            PlayerManager.instance.PlayerRigid.linearVelocityX = Speed;
+            //transform.Translate(Speed*Time.deltaTime, 0, 0);
         }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            PlayerManager.instance.PlayerRigid.linearVelocityX = 0;
+        }
+
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-Speed * Time.deltaTime, 0, 0);
+            PlayerManager.instance.PlayerRigid.linearVelocityX = -Speed;
+            //transform.Translate(-Speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKeyDown((KeyCode)PlayerPrefs.GetInt("JumpKey"))){
+        else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+            PlayerManager.instance.PlayerRigid.linearVelocityX = 0;
+        }
+
+
+        if (Input.GetKeyDown((KeyCode)PlayerPrefs.GetInt("JumpKey"))&&JumpTrigger){
+            JumpTrigger = false;
             PlayerManager.instance.PlayerRigid.AddForce(Vector2.up*JumpPower);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            JumpTrigger = true;
         }
     }
 }
